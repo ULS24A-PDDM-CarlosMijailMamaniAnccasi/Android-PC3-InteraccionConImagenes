71b37ff831b687b96a62aed9d4d2a8013d5204d6
Carlos Mijail Mamani Anccasi <cmamania@ulasalle.edu.pe>
2024-04-09 11:48:20 -0500

    update MainActivity.kt, limpiamos y agregamos funcionalidad a el boton y a la entrada e texto.

--- app/src/main/java/com/example/pc3_interaccionconimagenes/MainActivity.kt
+++ app/src/main/java/com/example/pc3_interaccionconimagenes/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.pc3_interaccionconimagenes
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val txt_custom = findViewById<EditText>(R.id.txt_custom)
+        val btn_imgb = findViewById<ImageButton>(R.id.imgb_main)
+        btn_imgb.setOnClickListener() {
+            //val msg = "Acabas de hacer click en la imagen :)"
+            Toast.makeText(this, txt_custom.text, Toast.LENGTH_SHORT).show()
         }
     }
 }
